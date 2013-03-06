c96afce324733b22a228f2ba97607e8b51309bdd
David Singleton <david@dsingleton.co.uk>
2013-03-06 13:41:14 +0000

    Add an offline copy of jQuery as a fallback, group CSS/JS by dir now we have a few JS files. Update path references for CSS/JS

--- generate.py
+++ generate.py
@@ -3,5 +3,5 @@
 import os, glob, json
 
 icons = json.dumps([os.path.basename(icon) for icon in glob.glob('icons/*.png')])
-open('icon_data.js', 'w').write('var icon_data = ' + icons)
+open('js/icon_data.js', 'w').write('var icon_data = ' + icons)
 
